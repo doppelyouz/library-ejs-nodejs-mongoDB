691a83da69504810c69f3fd340b4be97315ad036
anikisana <younker12345@gmail.com>
2022-12-15 21:50:17 +0600

    mongoose

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const mongoose = require('mongoose');
 
 const booksRoutes = require("./routes/book")
 
 const app = express();
-
+mongoose.connect('mongodb+srv://doppelyouz:sdGHhSL4Mnb6mZqv@cluster0.tt7avdq.mongodb.net/library?retryWrites=true&w=majority',() => {
+  console.log('mongo connected');
+}, (err) => {
+  console.log(err);
+})
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
